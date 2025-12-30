569cc17838d66b0ff7dfd9216d612403e6ad923d
Anlh-ogr <9.10hoangan.13072003@gmail.com>
2025-12-30 16:28:51 +0700

    Tạo một đầu nối API gọi các router từ thư mục
    Test với endpoind (health)

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -1,1 +1,10 @@
 # create FastAPI app + include routers [page:0]
+""" Tạo một đầu nối API FastAPI
+ * gọi các router từ thư mục """
+ 
+from fastapi import FastAPI
+from app.api.routes import health as health_router
+
+# Tạo app FastAPI và gọi router từ health
+app = FastAPI()
+app.include_router(health_router.router, prefix="/api")
