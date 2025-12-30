46f1d337716f3dd3f21fe5ab052931e204b3c4de
Anlh-ogr <9.10hoangan.13072003@gmail.com>
2025-12-30 16:03:44 +0700

    Lưu trữ DB URL
    Truy vấn .env.local
    Mã hóa thông tin trong DB
    Làm nền tảng truy cập DB cho session

--- apps/api/app/core/config.py
+++ apps/api/app/core/config.py
@@ -1,17 +1,39 @@
-# Trung tâm cấu hình ứng dụng
-""" Path Json - ENV(dev/prod) - ON/OFF API - Test Mode """
+""" Cấu hình kết nối DB từ .env.local để gọi URL DB
+Nạp biến môi trường thông qua os truy cập hệ thống và lấy biến môi trường.
 
-from dataclasses import dataclass       # định nghĩa class cấu hình gọn nhẹ
-from pathlib import Path                # quản lý path
-import os                               # đọc biến môi trường os (phân biệt dev/prod/test)
+Để tránh lộ thông tin, không hardcode trực tiếp trong env.local, lấy biến 
+cấu hình từ env.local thông qua lớp Settings và mã hóa thông tin nhạy cảm.
+    
+Dùng lru_cache để cache biến cấu hình tránh gọi nhiều lần. """
 
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+from pydantic import BaseModel, SecretStr
+from functools import lru_cache
 
-# Gán frozen = True để tránh thay đổi thuộc tính sau khi khởi tạo trong dataclass lib
-@dataclass(frozen=True)
-class Setting:
-    """ Central configuration for phase 0 to 2 (json database) """
-    APP_ENV: str = os.getenv("APP_ENV", "local")
-    # Default path DBjson : apps/api/app/data/circuit_scope.json
-    DB_PATH: Path = Path(os.getenv("DB_PATH", Path(__file__).resolve().parents[1] / "data" / "circuit_scope.json"))
+# Nạp môi trường từ file .env.local
+load_dotenv(dotenv_path=Path(".env.local"))
 
-settings = Setting() 
+""" backup cấu hình cứng nếu không có biến môi trường 
+dotenv_path = Path(__file__).resolve().parents[2] / ".env.local"
+load_dotenv(dotenv_path=dotenv_path)
+"""
+
+""" Lớp cấu hình Settings để lấy biến môi trường từ .env.local """
+class Settings(BaseModel):
+    database_url: SecretStr
+    
+    @property
+    def db_url(self) -> str:
+        """ Trả về chuỗi kết nối DB dưới dạng str """
+        return self.database_url.get_secret_value()
+
+@lru_cache()
+def get_settings() -> Settings:
+    url = os.getenv("DATABASE_URL")
+    if not url:
+        raise RuntimeError("DATABASE_URL not set in environment variables")
+    return Settings(database_url=url)
+
+settings = get_settings()
